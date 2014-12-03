f54239526342471960c2305bd53b0492882021fc
cpousamarcote <cpousamarcote@danielcastelao.org>
2014-12-03 10:36:15 +0100

    Ejercicio de Repaso 2.

---
+++ repaso2/Repaso2.java
@@ -0,0 +1,21 @@
+/*
+ * Programa en Java que cree 4 hilos que acceden a una variable compartida que es un contador.
+ * Cada uno de los hilos accede cuatro veces al contador y aumenta en una unidad su valor.
+ */
+package repaso2;
+
+/**
+ *
+ * @author findag
+ */
+public class Repaso2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        
+    }
+    
+}
