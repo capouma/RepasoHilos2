b9657565c9278d7eee377c7193e59968c9813819
cpousamarcote <cpousamarcote@danielcastelao.org>
2014-12-03 10:45:47 +0100

    Creamos nuestra clase Hilo que extendera de Thread

--- repaso2/Repaso2.java
+++ repaso2/Repaso2.java
@@ -16,10 +16,10 @@ public class Repaso2 {
     public static void main(String[] args)
     {
         // Creamos 4 hilos distintos.
-        Hilo hilo1 = new Hilo();
-        Hilo hilo2 = new Hilo();
-        Hilo hilo3 = new Hilo();
-        Hilo hilo4 = new Hilo();
+        Hilo hilo1 = new Hilo(1);
+        Hilo hilo2 = new Hilo(2);
+        Hilo hilo3 = new Hilo(3);
+        Hilo hilo4 = new Hilo(4);
         
         // Lanzamos los hilos.
         hilo1.start();
